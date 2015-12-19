8afd2cd335a8c4f1d0ec8cdf5e50e1a1cae15055
anu <anub018@gmail.com>
2015-12-19 21:06:54 +0530

    Added text-to-speech functionality

--- app/src/main/java/com/example/anubansal/text_to_speech/MainActivity.java
+++ app/src/main/java/com/example/anubansal/text_to_speech/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.anubansal.text_to_speech;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private String TAG = "TextToSpeech";
+    private TextToSpeech tts;
+    private Button btnSpeak;
+    private EditText typeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        tts = new TextToSpeech(this, this);
+
+        btnSpeak = (Button) findViewById(R.id.btnSpeak);
+
+        typeText = (EditText) findViewById(R.id.type_text);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                speakOut();
             }
         });
     }
@@ -48,5 +73,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+//            to change the language, use tts.setLanguage("lang");
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e(TAG, "This language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+        } else {
+            Log.e(TAG, "Initialization failed!");
+        }
+    }
+
+    private void speakOut() {
+        String text = typeText.getText().toString();
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+
+        super.onDestroy();
     }
 }
